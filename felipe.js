ee6a0d73ed5fd53c6a6adff8a7d82bbfc792a269
JonaRiosBrambila <95506962+JonaRiosBrambila@users.noreply.github.com>
2022-01-14 16:53:45 -0600

    Desafio Felipe 1

---
+++ felipe.js
@@ -0,0 +1,15 @@
+var  nombres = ["Lucía", "Maria", "Jesus", "karen", "Andrea", "José"];
+
+console.log(nombres);
+
+let nuevaLongitud = nombres.unshift('Andrea')
+console.log(nombres);
+
+let ultimo = nombres.pop()
+console.log(nombres);
+
+let nuevaLongitud1 = nombres.push('Cristobal', 'Fernanda', 'Armando')
+console.log(nombres);
+
+let nuevaLongitud3 = nombres.unshift('Federico')
+console.log(nombres);
